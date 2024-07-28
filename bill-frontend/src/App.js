8b07cf3eafd19418343a97b7c2c53c6ef4f4070b
BriLeighk <kirchgessner@wisc.edu>
2024-07-28 12:16:34 -0400

    removed error

--- bill-frontend/src/App.js
+++ bill-frontend/src/App.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MainPage from './components/MainPage';
-import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BillForm from './components/BillForm';
+import BillDashboard from './components/BillDashboard';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Switch>
-                    <Route path="/components/main" component={MainPage} />
-                    <Route path="/components/dashboard" component={Dashboard} />
-                    <Route path="/" exact component={MainPage} />
-                    {/* Add more routes as needed */}
-                </Switch>
+                <Routes>
+                    {/* Route to the main page */}
+                    <Route path="/" element={<BillForm />} />
+                    {/* Route to the dashboard on submission of form */}
+                    <Route path="/dashboard" element={<BillDashboard />} />
+                </Routes>
                 <footer className="App-footer">
                     <div className="footer-content">
                         <div className="google-form-container">
-                            <iframe
-                                src="https://docs.google.com/forms/d/e/1FAIpQLSdjqyMGtNWsc6rPZH3Wb5mwTj1Wmh74amPwTONvpg9qJXFJZg/viewform?embedded=true"
-                                width="100%"
-                                height="418"
-                                frameBorder="0"
-                                marginHeight="0"
-                                marginWidth="0"
-                                title="Google Form"
-                            >
-                                Loading…
-                            </iframe>
+                            
                         </div>
                         <p className="trademark">@ Bri Kirchgessner | Ayen Monasha | Tridhatri Vallamkondu</p>
                     </div>
